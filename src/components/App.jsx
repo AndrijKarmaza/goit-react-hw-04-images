704191cf86300d74416c6e6bba4db24f21be3b5e
AndrijKarmaza <andrij.karmaza@gmail.com>
2023-06-06 13:10:05 +0300

    download task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,10 @@
+// import { Button } from './Button/Button';
+// import { ImageGallery } from './ImageGallery/ImageGallery';
+// import { ImageGalleryItem } from './ImageGalleryItem/ImageGalleryItem';
+// import { Loader } from './Loader/Loader';
+// import { Modal } from './Modal/Modal';
+// import { Searchbar } from './Searchbar/Searchbar';
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+  return <div></div>;
 };
